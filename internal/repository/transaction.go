f30bf6e7cadff7ff31ef2496ffae3b896b3a6c63
Putri Lee <fatahnuram@gmail.com>
2026-02-08 17:59:31 +0700

    feat: optimize query for fetch products before trx

--- internal/repository/transaction.go
+++ internal/repository/transaction.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 
 	"github.com/fatahnuram/learn-go-kasir-api/internal/dto"
 	"github.com/fatahnuram/learn-go-kasir-api/internal/model"
@@ -26,11 +27,6 @@ func (r *TransactionRepository) Checkout(items []dto.CheckoutItem) (*model.Trans
 	}
 	defer tx.Rollback()
 
-	// getSelectedProductsQuery := `
-	// SELECT id, name, price, stock
-	// FROM products
-	// WHERE id IN `
-
 	// createTrxDetailsQuery := `
 	// INSERT INTO transaction_details (transaction_id, product_id, quantity, subtotal)
 	// VALUES `
@@ -40,26 +36,27 @@ func (r *TransactionRepository) Checkout(items []dto.CheckoutItem) (*model.Trans
 	// SET stock = $1
 	// WHERE id = $2`
 
+	// fetch related products then process programmatically to avoid n+1 query
+	products, err := r.fetchSelectedProductsToMap(items)
+	if err != nil {
+		return nil, err
+	}
+
 	total := 0
 	details := make([]model.TransactionDetails, 0)
 
 	for _, item := range items {
-		var price, stock int
-		var name string
-
-		err := tx.QueryRow(`SELECT name, price, stock FROM products WHERE id = $1`, item.ProductID).Scan(&name, &price, &stock)
-		if err == sql.ErrNoRows {
+		p, ok := products[item.ProductID]
+		if !ok {
+			// not ok means product not found
 			return nil, fmt.Errorf("product not found, ID: %d", item.ProductID)
 		}
-		if err != nil {
-			return nil, err
+
+		if p.Stock < item.Qty {
+			return nil, fmt.Errorf("insufficient stock, product ID: %d, want: %d, have: %d", item.ProductID, item.Qty, p.Stock)
 		}
 
-		if stock < item.Qty {
-			return nil, fmt.Errorf("insufficient stock, product ID: %d, want: %d, have: %d", item.ProductID, item.Qty, stock)
-		}
-
-		subtotal := price * item.Qty
+		subtotal := p.Price * item.Qty
 		total += subtotal
 
 		_, err = tx.Exec(`UPDATE products SET stock = stock - $1 WHERE id = $2`, item.Qty, item.ProductID)
@@ -69,7 +66,7 @@ func (r *TransactionRepository) Checkout(items []dto.CheckoutItem) (*model.Trans
 
 		details = append(details, model.TransactionDetails{
 			ProductID:   item.ProductID,
-			ProductName: name,
+			ProductName: p.Name,
 			Qty:         item.Qty,
 			Subtotal:    subtotal,
 		})
@@ -101,4 +98,35 @@ func (r *TransactionRepository) Checkout(items []dto.CheckoutItem) (*model.Trans
 		TotalAmount: total,
 		Details:     details,
 	}, nil
+}
+
+func (r *TransactionRepository) fetchSelectedProductsToMap(items []dto.CheckoutItem) (map[int]*model.Product, error) {
+	q := `SELECT id, name, price, stock
+	FROM products
+	WHERE id IN (%s)`
+
+	idplaceholder := make([]string, len(items))
+	idvalues := make([]interface{}, len(items))
+	products := make(map[int]*model.Product, len(items))
+
+	for i := range items {
+		idplaceholder[i] = fmt.Sprintf("$%d", i+1)
+		idvalues[i] = items[i].ProductID
+	}
+
+	rows, err := r.Db.Query(fmt.Sprintf(q, strings.Join(idplaceholder, ",")), idvalues...)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var p model.Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price, &p.Stock)
+		if err != nil {
+			return nil, err
+		}
+		products[p.ID] = &p
+	}
+
+	return products, nil
 }
